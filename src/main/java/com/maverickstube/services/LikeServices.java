575e6d90402fd5e0c4f4d21465e18dad00a95603
Precious <preciousonome002@gmail.com>
2024-07-01 10:52:08 +0100

    mavericksHub

--- src/main/java/com/maverickstube/services/LikeServices.java
+++ src/main/java/com/maverickstube/services/LikeServices.java
@@ -1,7 +1,8 @@
 package com.maverickstube.services;
 
-import com.maverickstube.maverickshub.dtos.requests.LikeMediaRequest;
-import com.maverickstube.maverickshub.dtos.responses.LikeResponse;
+
+import com.maverickstube.dtos.requests.LikeMediaRequest;
+import com.maverickstube.dtos.responses.LikeResponse;
 
 public interface LikeServices {
     LikeResponse likeMedia(LikeMediaRequest request);
